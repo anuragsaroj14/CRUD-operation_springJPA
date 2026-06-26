49cea2c2a09cd3636263a700d145690e41fb4b84
Anurag Saroj <159571842+anuragsaroj14@users.noreply.github.com>
2026-06-26 21:14:39 +0530

    Add files via upload

---
+++ Bootpro01CurdOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootpro01CurdOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootpro01CurdOperationsApplication.class, args);
+	}
+
+}
